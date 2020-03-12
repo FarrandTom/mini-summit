2e9e05dcdee6be77dde3682c753321477c71efca
Tom Farrand <33008723+FarrandTom@users.noreply.github.com>
2020-03-12 17:51:08 +0000

    Add jupyterlab url

--- new_vpc_cluster/outputs.tf
+++ new_vpc_cluster/outputs.tf
@@ -2,6 +2,11 @@ output "wmlce_token" {
   value = "${random_password.wmlce_token.result}"
 }
 
+output "jupyterlab_url" {
+    value = "http://${ibm_is_floating_ip.fip1.address}:443/lab"
+}
+
+
 output "vm_private_key" {
   value = "\n${tls_private_key.vision_keypair.private_key_pem}"
 }
