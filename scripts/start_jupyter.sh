3d630b7e04e3a397337aa59d15c5feb67a825a48
Tom Farrand <33008723+FarrandTom@users.noreply.github.com>
2020-03-12 11:27:06 +0000

    Update start_jupyter.sh

--- scripts/start_jupyter.sh
+++ scripts/start_jupyter.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
 
+RUN_DIR=/home/volume
+
 source /home/volume/anaconda3/etc/profile.d/conda.sh
 conda activate wmlce_env
 
 ## Start Jupyter Lab
 echo "Starting Jupyter Lab..."
+cd ${RUN_DIR}
 nohup jupyter lab --ip=0.0.0.0 --allow-root --port 443 --no-browser --NotebookApp.token="$1" > error.log &
 echo "Jupyter lab up and running!"
