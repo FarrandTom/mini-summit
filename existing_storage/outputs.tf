a1efc7b472a163d94f6a37250cb5289c4c5b77ce
Tom Farrand <33008723+FarrandTom@users.noreply.github.com>
2020-03-12 13:40:17 +0000

    Update outputs.tf

--- existing_storage/outputs.tf
+++ existing_storage/outputs.tf
@@ -2,6 +2,10 @@ output "wmlce_token" {
   value = "${random_password.wmlce_token.result}"
 }
 
+output "wmlce_url" {
+    value = "https://${ibm_is_floating_ip.fip1.address}:443/token"
+}
+
 output "vm_private_key" {
   value = "\n${tls_private_key.vision_keypair.private_key_pem}"
 }
